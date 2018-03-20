3ddf14f66211c1f4b1b5bd23d8d862cb4719a230
jun59 <jun59@163.com>
2018-03-20 18:07:24 +0800

    新增 spring config client

--- ag-jpa/src/main/java/com/jpa/controller/TbAdminController.java
+++ ag-jpa/src/main/java/com/jpa/controller/TbAdminController.java
@@ -1,6 +1,7 @@
 package com.jpa.controller;
 
 import com.jpa.entity.TbAdmin;
+
 import com.jpa.service.TbAdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
